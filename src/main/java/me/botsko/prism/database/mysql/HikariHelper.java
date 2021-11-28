7517376829f7d3f92237d272d0c16f4f8c8a8acf
repairnator <noreply@github.com>
2021-11-28 06:23:42 +0000

    Proposal for patching Sorald rule 2095

--- src/main/java/me/botsko/prism/database/mysql/HikariHelper.java
+++ src/main/java/me/botsko/prism/database/mysql/HikariHelper.java
@@ -40,16 +40,14 @@ class HikariHelper {
                     prop.setProperty("dataSource." + name, val);
                 }
             }
-            try {
-                if (!propFile.getParentFile().exists() && !propFile.getParentFile().mkdirs()) {
-                    Prism.log("Prism Directory couldn't be created");
-                }
-                OutputStream out = new FileOutputStream(propFile);
-                prop.store(out, "Prism Hikari Datasource Properties for"
-                        + " advanced database Configuration");
-                Prism.log("Database Configuration saved to - " + propFile.getPath());
-            } catch (IOException e) {
-                Prism.log("Could not save Hikari.properties - " + e.getMessage());
+        try (OutputStream out = new FileOutputStream(propFile)) {
+            if (!propFile.getParentFile().exists() && !propFile.getParentFile().mkdirs()) {
+                Prism.log("Prism Directory couldn't be created");
             }
+            prop.store(out, "Prism Hikari Datasource Properties for" + " advanced database Configuration");
+            Prism.log("Database Configuration saved to - " + propFile.getPath());
+        } catch (IOException e) {
+            Prism.log("Could not save Hikari.properties - " + e.getMessage());
+        }
         }
 }
